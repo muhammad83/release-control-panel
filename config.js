cfaa9fe8cafd1a486e05f2db91ecafabaac4586a
Muhammad Qureshi <muhammad.qureshi@digital.hmrc.gov.uk>
2015-12-07 09:28:04 +0000

    updated config file

--- config.js
+++ config.js
@@ -1,7 +1,21 @@
-module.exports = {
+var config = {
     username: "",
     password: "",
     jiraUrl: "https://jira.tools.tax.service.gov.uk",
     prodLeftUrl: "https://releases.tax.service.gov.uk/env/prod-left-a",
     prodRightUrl: "https://releases.tax.service.gov.uk/env/prod-right"
 };
+
+if (!config.username || config.username.length == 0)
+{
+    console.error("Username not set.");
+    throw new Error("Username not set.");
+}
+
+if (!config.password || config.password.length == 0)
+{
+    console.error("Password not set.");
+    throw new Error("Password not set.");
+}
+
+module.exports = config;
