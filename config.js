801479c3735cd338311c2a6324f59cf77d2492b1
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-01-14 08:56:15 +0000

    Removing username and password.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    username: "tomasz.mikus",
-    password: "pN4Fr{mrsiWqdqR3QWZYsiiGW",
+    username: "",
+    password: "",
     jiraUrl: "https://jira.tools.tax.service.gov.uk",
     prodLeftUrl: "https://releases.tax.service.gov.uk/env/prod-left-a",
     prodRightUrl: "https://releases.tax.service.gov.uk/env/prod-right"
