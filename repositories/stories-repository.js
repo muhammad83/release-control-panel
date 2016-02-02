d70f676787181d183d75fd5f1b52389d9f174588
Tomasz Mikus <mikus.tomasz@gmail.com>
2016-02-02 11:41:20 +0000

    Fixing problem with searching for JIRA tickets.

--- repositories/stories-repository.js
+++ repositories/stories-repository.js
@@ -120,7 +120,12 @@ class StoriesRepository
     static prepareJQLForTags(projectsAndTags, jiraNumbers)
     {
         const separator = ", ";
-        let gitTags = projectsAndTags.map(projectAndTags => projectAndTags.tags.map(tag => projectAndTags.name + "-" + tag).join(separator)).join(separator);
+
+        let gitTags = projectsAndTags
+            .filter(project => project.tags.length)
+            .map(projectAndTags => projectAndTags.tags.map(tag => projectAndTags.name + "-" + tag).join(separator))
+            .join(separator);
+        
         if (jiraNumbers.length > 0)
         {
             let jiraNumbersString = jiraNumbers.join(separator);
